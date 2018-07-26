9700f275a7540e595bea2e55828c42a20c8be6cd
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2018-07-26 18:00:44 -0500

    create new daily trigger, main function now moves old report into old folder

--- main.ts
+++ main.ts
@@ -1,21 +1,39 @@
 const Constants = {
-    baseFolder: "daily_reports"
+    baseFolder: "daily_reports",
+    oldFolder: "old_reports"
+}
+
+function createDailyTrigger() {
+    ScriptApp.newTrigger('createNewReport')
+        .timeBased()
+        .onWeekDay(ScriptApp.WeekDay.MONDAY)
+        .onWeekDay(ScriptApp.WeekDay.TUESDAY)
+        .onWeekDay(ScriptApp.WeekDay.WEDNESDAY)
+        .onWeekDay(ScriptApp.WeekDay.THURSDAY)
+        .onWeekDay(ScriptApp.WeekDay.FRIDAY)
+        .atHour(8)
+        .create()
 }
 
 function createNewReport() {
     let previousReportId = []
     const baseFolders = DriveApp.getFoldersByName(Constants.baseFolder)
+    const oldFolder = DriveApp.getFoldersByName(Constants.oldFolder)
     while (baseFolders.hasNext()) { 
         const folder = baseFolders.next()
+        const old = oldFolder.next()
         const files = folder.getFiles()
         while (files.hasNext()) {
             previousReportId.push(files.next().getId())
         }
+        const report = DriveApp.getFileById(previousReportId[0])
+        old.addFile(report)
         let today = new Date()
         const dd = today.getDate()
         const mm = ('0' + (today.getMonth() + 1)).slice(-2)
         let newReportTitle = mm + "/" + dd + " Daily Report"
-        DriveApp.getFileById(previousReportId[0]).makeCopy(newReportTitle, folder)
+        report.makeCopy(newReportTitle, folder)
+        folder.removeFile(report)
     }
 }
 
